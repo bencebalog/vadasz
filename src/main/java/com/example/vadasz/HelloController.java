33830b93f8f22716e288e750e7b609b9eed91cb5
Balog Bence <balogbence991@gmail.com>
2024-02-20 15:51:42 +0100

    Vadaszat

--- src/main/java/com/example/vadasz/HelloController.java
+++ src/main/java/com/example/vadasz/HelloController.java
@@ -1,5 +1,7 @@
 package com.example.vadasz;
 
+import javafx.animation.Animation;
+import javafx.animation.AnimationTimer;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
@@ -10,19 +12,29 @@ public class HelloController {
     @FXML private Pane pnJatek;
     @FXML private Label lbLoves;
     @FXML private Label lbRoka;
-    private String[] iconNev = {"dark","dead","fox","fox","home","tree"};
+    private String[] iconNev = {"dark","dead","fox","home","tree"};
     private Image[] icon = new Image[5];
 
     private final int DARK = 0;
-    private final int ROKA = 0;
-    private final int TREE = 0;
+    private final int DEAD = 1;
+    private final int ROKA = 2;
+    private final int HOME = 3;
+    private final int TREE = 4;
 
     private ImageView[][] lt= new ImageView[16][32];
     private int[][]  t=new int[16][32];
     private int es=-1,eo=-1;
 
+    private AnimationTimer timer = null;
+    private long tt = 0;private long most = 0;
+
+
     private int roka = 0;
     private int rokaMax = 0;
+    private int loves = 0;
+    private int talalt=0;
+
+
     public void initialize(){
         for(int i=0;i<5;i++) icon[i] = new Image(getClass().getResourceAsStream("icons/"+iconNev[i]+".png"));
         for(int s=0;s<16;s++) for(int o=0;o<32;o++){
@@ -31,17 +43,27 @@ public class HelloController {
             lt[s][o].setLayoutX(10+o*48);
             lt[s][o].setLayoutY(10+s*48);
             lt[s][o].setOnMouseEntered(e -> vilagit(ss,oo));
+            lt[s][o].setOnMousePressed(e -> loves(ss,oo));
             pnJatek.getChildren().add(lt[s][o]);
         }
         generalErdo();
+        timer=new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                most = now;
+                if(now>tt) elbujik();
+            }
+        };
+        timer.start();
     }
 
     private void generalErdo(){
-        roka=0;
+        roka=0;loves=0;talalt=0;
         for(int s=0;s<16;s++) for(int o=0;o<32;o++){
             if(Math.random()<0.1) {t[s][o] = ROKA; roka++;} else t[s][o] = TREE;
         }
         rokaMax=roka;
+        lbLoves.setText(loves+" lövés / "+talalt+" találat");
         lbRoka.setText(roka+" / "+rokaMax+" róka");
     }
 
@@ -50,16 +72,39 @@ public class HelloController {
             for(int dS=-2;dS<=+2;dS++)for(int dO=-2;dO<=+2;dO++){
                 int ss=es+dS,oo=eo+dO;
                 if(ss>=0 && ss<=15 && oo>=0 && oo<=31 && !(Math.abs(dS)==2 && Math.abs(dO)==2)){
-                    lt[ss][oo].setImage(icon[t[ss][oo]]);
+                    lt[ss][oo].setImage(icon[DARK]);
                 }
             }
             for(int dS=-2;dS<=+2;dS++)for(int dO=-2;dO<=+2;dO++){
                 int ss=s+dS,oo=o+dO;
                 if(ss>=0 && ss<=15 && oo>=0 && oo<=31 && !(Math.abs(dS)==2 && Math.abs(dO)==2)){
-                    lt[ss][oo].setImage(icon[DARK]);
+                    lt[ss][oo].setImage(icon[t[ss][oo]]);
                 }
             }
             es=s;eo=o;
+            tt = most+500000000;
         }
+    }
+    private void elbujik(){
+        for(int dS=-2;dS<=+2;dS++)for(int dO=-2;dO<=+2;dO++) {
+            int ss = es + dS, oo = eo + dO;
+            if (ss >= 0 && ss <= 15 && oo >= 0 && oo <= 31 && !(Math.abs(dS) == 2 && Math.abs(dO) == 2) && t[ss][oo] == ROKA) {
+                lt[ss][oo].setImage(icon[HOME]);
+                t[ss][oo] = HOME;
+                roka--;
+            }
+        }
+        lbRoka.setText(roka+" / "+rokaMax+" róka");
+    }
+
+    private void loves(int s,int o){
+        loves++;
+        if(t[s][o] == ROKA){
+            lt[s][o].setImage(icon[DEAD]);t[s][o]=DEAD;roka--;
+            lbRoka.setText(roka+" / "+rokaMax+" róka");
+            talalt++;
+
+        }
+        lbLoves.setText(loves+" lövés / "+talalt+" találat");
     }
 }
